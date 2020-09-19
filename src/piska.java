70ab5948419e466e076cd9986ceeaaea0405313d
piska228AYE <a4a43@yandex.ru>
2020-09-19 17:36:18 +0400

    A+B
    aaaaaaaaaaaaaaaaaa

--- src/piska.java
+++ src/piska.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class piska {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введите текст:");
-        System.out.println(sc.nextLine());
+        System.out.print("Введите число A:");
+        int num1 = sc.nextInt();
+        System.out.print("Введите число B:");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
     }
 }
