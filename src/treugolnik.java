323146f254e4d6064db49a8b717a48ef430d8de1
vova22812345 <a4a43@yandex.ru>
2020-09-24 20:04:16 +0400

    chtob bilo krasiva

--- src/treugolnik.java
+++ src/treugolnik.java
@@ -13,25 +13,25 @@ public class treugolnik {
         double x3 = sc.nextInt();
         double y3 = sc.nextInt();
         double a = Math.sqrt(Math.pow(x1 - x2,2) + Math.pow(y1 - y2,2));
-        System.out.println();
-        System.out.print("Длинна стороны ab- ");
+        System.out.println("Результаты:");
+        System.out.print("Длина стороны ab ");
         System.out.printf("%.3f", a);
         double b = Math.sqrt(Math.pow(x2 - x3,2) + Math.pow(y2 - y3,2));
         System.out.println();
-        System.out.print("Длинна стороны bc - ");
+        System.out.print("Длина стороны bc ");
         System.out.printf("%.3f", b);
         double c = Math.sqrt(Math.pow(x3 - x1,2) + Math.pow(y3 - y1,2));
         System.out.println();
-        System.out.print("Длинна стороны ca - ");
+        System.out.print("Длина стороны ca ");
         System.out.printf("%.3f", c);
         double P = a + b + c;
         System.out.println();
-        System.out.print("Периметр треугольника ABC - ");
+        System.out.print("Периметр треугольника ABC ");
         System.out.printf("%.3f", P);
         double p = P * 0.5;
         double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         System.out.println();
-        System.out.print("Площадь треугольника ABC - ");
+        System.out.print("Площадь треугольника ABC ");
         System.out.printf("%.3f", S);
     }
 }
