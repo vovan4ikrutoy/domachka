16dbd498309761aa315322922460e2b4aaeacf79
Chelinglasses <fedorkovyura2008@gmail.com>
2020-09-26 17:25:22 +0400

    test

--- src/test.java
+++ src/test.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class test {
 public static void main(String[] args){
-    boolean isSchoolTime = 5 > 4;
-    System.out.println(isSchoolTime);
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    if(x < 1000 && x >= 100 && x % 5 == 0){
+        System.out.println("yes");
+    } else {
+        System.out.println("no");
+    }
 }
 }
