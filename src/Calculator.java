638f046a968b44c3abd7d93f264b438de249d372
vova22812345 <a4a43@yandex.ru>
2020-09-26 13:00:49 +0400

    kalkulator v2

--- src/Calculator.java
+++ src/Calculator.java
@@ -19,17 +19,26 @@ public class Calculator {
         System.out.print("Укажите число:");
         return sc.nextDouble();
     }
+    public static double ugol()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Укажите угол:");
+        return sc.nextDouble();
+    }
     public static void main(String[] args){
         double num1;
         double num2;
         Scanner sc = new Scanner(System.in);
         System.out.println("Укажите действие:");
-        System.out.println("1 +");
-        System.out.println("2 -");
-        System.out.println("3 *");
-        System.out.println("4 /");
-        System.out.println("5 √");
-        System.out.println("6 ^");
+        System.out.print("1 + ");
+        System.out.print("2 - ");
+        System.out.println("3 * ");
+        System.out.print("4 / ");
+        System.out.print("5 √ ");
+        System.out.println("6 ^ ");
+        System.out.print("7 sin ");
+        System.out.print("8 cos ");
+        System.out.println("9 tg ");
         int dey = sc.nextInt();
         double result;
         if(dey == 1){
@@ -62,6 +71,27 @@ public class Calculator {
             int step = sc.nextInt();
             result = Math.pow(num1,step);
             System.out.println("Результат:" + result);
+        }else if(dey == 7) {
+            num1 = ugol();
+            if (num1 < 360 && num1 > 0){
+                num1 = Math.toRadians(num1);
+                result = Math.sin(num1);
+                System.out.println("Результат:" + result);
+            }
+        }else if(dey == 8) {
+            num1 = ugol();
+            if (num1 < 360 && num1 > 0){
+                num1 = Math.toRadians(num1);
+                result = Math.cos(num1);
+                System.out.println("Результат:" + result);
+            }
+        }else if(dey == 9) {
+            num1 = ugol();
+            if (num1 < 360 && num1 > 0){
+                num1 = Math.toRadians(num1);
+                result = Math.tan(num1);
+                System.out.println("Результат:" + result);
+            }
         }else{
             System.out.println("error#@000000000000rrt24542");
             System.out.println("w45694q37b85gc46&#^cbg)$&^");
