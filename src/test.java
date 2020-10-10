26dea512487d656b254ff9cadb281104f572bf38
vova22812345 <a4a43@yandex.ru>
2020-10-10 17:30:10 +0400

    xz

--- src/test.java
+++ src/test.java
@@ -1,13 +1,34 @@
 import java.util.Scanner;
 
 public class test {
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
-    if(x < 1000 && x >= 100 && x % 5 == 0){
-        System.out.println("yes");
-    } else {
-        System.out.println("no");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        n = sc.nextInt();
+        char[][] a = new char[n][n];
+        if (n % 2 != 0) {
+            //обработка
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == j) {
+                        a[i][j] = '*';
+                    } else if (i == n / 2 || j == n / 2) {
+                        a[i][j] = '*';
+                    } else if (i == n - j - 1) {
+                        a[i][j] = '*';
+                    } else {
+                        a[i][j] = '.';
+                    }
+                }
+            }
+            //вывод
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(a[i][j] + " ");
+                }
+                System.out.println(" ");
+            }
+        }
     }
 }
-}
+
--- src/test2.java
+++ src/test2.java
@@ -1,44 +1,20 @@
 import java.util.Scanner;
 
 public class test2 {
+
+    public static int gig(int X) {
+        int num = 0;
+        for(int i = 100; i < X; i++){
+            if((i / 100 + i / 10 % 10 + i % 10) % 13 == 0){
+                num++;
+            }
+        }
+        return num;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Выберите действие");
-        System.out.println("1 Куб");
-        System.out.println("2 Параллелепипед");
-        System.out.println("3 Сфера");
-        System.out.println("4 Конус");
-        System.out.println("5 Цилиндр");
-        int dey = sc.nextInt();
-        double num1 = 0;
-        double num2 = 0;
-        double num3 = 0;
-        switch (dey) {
-
-            //куб
-            case 1:
-                num1 = sc.nextDouble();
-                System.out.println(Math.pow(num1,3));
-                break;
-
-            //Параллелепипед
-            case 2:
-                num1 = sc.nextDouble();
-                num2 = sc.nextDouble();
-                num3 = sc.nextDouble();
-                System.out.println(num1 * num2 * num3);
-                break;
-            //сфера
-            case 3:
-                num1 = sc.nextDouble();
-                System.out.println(1.33333333333 * 3.1415926535 * Math.pow(num1,3));
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-        }
+        int X = sc.nextInt();
+        System.out.println(gig(X));
     }
 }
